b8a43c4687117ce0ce0d40fbbdc4bdf7ffc73396
Zulfat Ilysov <mail@zulfat.net>
2015-04-16 23:57:32 +0300

    server proxy

--- client/server.js
+++ client/server.js
@@ -1,11 +1,19 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var request = require('request');
 
 var app = new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
+});
+
+var apiUrl = 'http://192.168.0.2:3200';
+app.use('/api', function(req, res) {
+    console.log(req.url);
+    var url = apiUrl + '/api' + req.url;
+    req.pipe(request(url)).pipe(res);
 });
 
 var port = 3000;
