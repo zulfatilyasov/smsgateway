3bb7d0361a3d3a952abd6a14c23ec7ad9931cd8b
Zulfat Ilysov <mail@zulfat.net>
2015-04-18 02:14:07 +0300

    setting up codeship ci

--- server/server.js
+++ server/server.js
@@ -20,8 +20,6 @@ app.use(loopback.logger('dev'));
 app.use(loopback.static(path.join(__dirname, '..', 'client', 'build')));
 
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
     if (err) throw err;
 
