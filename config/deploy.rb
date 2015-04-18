d5f1f0a7f749c581befe3226c99d961ea45bc09d
Zulfat Ilysov <mail@zulfat.net>
2015-04-18 22:45:08 +0300

    capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,6 @@ set :repo_url, 'git@bitbucket.org:zulfatilyasov/smsgateway.git'
 set :deploy_to, '/root/sms-gateway'
 set :app_command, 'server/server.js'
 set :pm2_name, 'smsgateway'
-# Default value for :scm is :git
-# set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
