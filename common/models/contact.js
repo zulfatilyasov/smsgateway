ca90f7d3d70cd435e01750db0507b8c7dd5ae2cc
Zulfat Ilyasov <mail@zulfat.net>
2015-05-17 00:19:39 +0300

    contacts api

---
+++ common/models/contact.js
@@ -0,0 +1,79 @@
+var _ = require('lodash');
+var ObjectId = require('mongodb').ObjectId;
+
+module.exports = function(Contact) {
+    Contact.observe('after save', function(ctx, next) {
+        var contact = ctx.instance;
+        if (!contact || !contact.groups || !contact.groups.length) {
+            next()
+            return;
+        }
+
+        var userId = contact.userId;
+        var groups = contact.groups;
+        var groupIds = _.pluck(groups, 'id');
+        var groupObjectIds = _.map(groupIds, function(stringId) {
+            return new ObjectId(stringId);
+        });
+        if (!groupIds || !groupIds.length) {
+            var err = new Error("Contact groups should have group ids");
+            err.statusCode = 422;
+            next(err);
+            return;
+        }
+        var db = Contact.dataSource.connector.db;
+        var GroupCollection = db.collection("Group");
+        GroupCollection.update({
+                _id: {
+                    "$in": groupObjectIds
+                }
+            }, {
+                "$addToSet": {
+                    "contacts": contact.id
+                }
+            }, {
+                multi: true
+            },
+            function(err, info) {
+                if (err) {
+                    next(err);
+                } else {
+                    next(null);
+                }
+            }
+        );
+    });
+
+    Contact.observe('before delete', function(ctx, next) {
+        if(!ctx.instance){
+            next();
+            return
+        }
+        var contactId = contactId;
+        var groupIds = _.pluck(ctx.instance.groups, 'id');
+        var groupObjectIds = _.map(groupIds, function(stringId) {
+            return new ObjectId(stringId);
+        });
+        var db = Contact.dataSource.connector.db;
+        var GroupCollection = db.collection("Group");
+        GroupCollection.update({
+                _id: {
+                    "$in": groupObjectIds
+                }
+            }, {
+                "$pull": {
+                    "contacts": contact.id
+                }
+            }, {
+                multi: true
+            },
+            function(err, info) {
+                if (err) {
+                    next(err);
+                } else {
+                    next(null);
+                }
+            }
+        );
+    });
+};
---
+++ common/models/group.js
@@ -0,0 +1,74 @@
+var q = require('Q');
+
+module.exports = function(Group) {
+    Group.observe('after save', function(ctx, next) {
+        var group = ctx.instance;
+        if (!group || group.isNewInstance) {
+            next();
+            return;
+        }
+
+        var db = Group.dataSource.connector.db;
+        var ContactCollection = db.collection("Contact");
+        
+        ContactCollection.update({
+            "groups.id": group.id.toString()
+        }, {
+            "$set": {
+                "groups.$.name": group.name
+            }
+        }, {
+            multi: true
+        }, function(err, info) {
+            if (err) {
+                next(err);
+            } else {
+                next(null);
+            }
+        });
+    });
+
+    Group.contacts = function(id, cb) {
+        Group.findById(id, function(err, group) {
+            if (err) {
+                cb(err);
+                return
+            }
+
+            var Contact = Group.app.models.Contact;
+            console.log(group.contacts);
+            Contact.find({
+                where: {
+                    id: {
+                        inq: group.contacts
+                    }
+                }
+            }, function(err, contacts) {
+                console.log(contacts);
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                cb(null, contacts);
+            });
+        });
+    };
+
+    Group.remoteMethod(
+        'contacts', {
+            accepts: [{
+                arg: 'id',
+                type: 'string',
+                required: true
+            }],
+            http: {
+                path: '/:id/contacts',
+                verb: 'get'
+            },
+            returns: {
+                arg: 'contacts',
+                type: 'array'
+            }
+        }
+    );
+};
